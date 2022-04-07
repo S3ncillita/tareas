293830fa4b96e18747568e98ec5472ae8e96df34
S3ncillita <fabian_noah@hotmail.com>
2022-04-07 13:46:48 -0400

    actualizado

---
+++ snpp-python/Tarea 7.1.py
@@ -0,0 +1,34 @@
+
+"""band = False
+while band==False:
+    contraseña = input("Introduce la contraseña: ")
+    if contraseña == "sesamo":
+        print("Pasa")
+        band=True
+    else:
+        print("No pasa")
+        band=False"""
+    
+
+"""def aplica_iva(base,iva =10):
+     base = base * iva/100     
+     return base
+
+base = int(input('Introduce la base imponible de la factura: '))
+print(aplica_iva(base))
+"""
+"""age=int((input("¿Cuál es tu edad?: ")))
+if age < 18: 
+ print("Eres menor de edad.") 
+else: 
+  print("Eres mayor de edad.")
+
+"""
+while True:
+     frase= str(input(("Introduce algo: ")))
+     print(frase)
+     if frase =="salir":
+       break
+     
+
+         
